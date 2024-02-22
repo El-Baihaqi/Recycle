2fb617ca54189ec811432ef126e594832866989a
baihaqi <asysyamsgithan07@gmail.com>
2024-02-22 15:02:55 +0700

    Initial commit

---
+++ app/src/main/java/com/www/recycle/hero.kt
@@ -0,0 +1,8 @@
+package com.www.recycle
+
+
+	data class Hero(
+        	        var name: String = "Glennn",
+        	        var from: String = "Manchester",
+        	        var photo: String = "")
+
